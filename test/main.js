649a0a0e73c22a996d2d234722a0981902681bb5
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-10 10:30:22 -0800

    a

--- test/main.js
+++ test/main.js
@@ -1,175 +1,146 @@
-// Create an array to store our particles
-var particles = [];
+$(function() {
+  "use strict";
+  var a = 0;
+  $('#tv').hide();
+  for (; a < 25; a += 1) {
+    setTimeout(function b() {
+      var a = Math.random() * 1e3 + 5e3,
+        c = $("<div />", {
+          "class": "smoke",
+          css: {
+            left: Math.random() * 800,
+            backgroundSize: "contain",
+            width: Math.random() * 800,
+            height: Math.random() * 600
+          }
+        });
+      $(c).addClass("animated");
+      $(c).addClass("zoomIn");
+      $(c).addClass("rollOut");
+      $(c).appendTo("#viewport");
+      $.when($(c).animate({}, {
+          duration: a / 4,
+          easing: "linear",
+          queue: false,
+          complete: function() {
+            $(c).animate({}, {
+              duration: a / 3,
+              easing: "linear",
+              queue: false
+            })
+          }
+        }),
+        $(c).animate({
+          bottom: $("#viewport").height()
+        }, {
+          duration: a,
+          easing: "linear",
+          queue: false
+        })).then(
+        function() {
+          $(c).remove();
+          b()
+        })
+    }, Math.random() * 3e3)
+  }
+  $("body").keypress(function() {
+    $('body').addClass("fadeOut");
+    setTimeout(function() {
+      $('#tv').show();
+    }, 1000);
 
-// The amount of particles to render
-var particleCount = 30;
+    console.log("Handler for .keypress() called.");
+  });
+}())
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tv,
+  playerDefaults = {
+    autoplay: 0,
+    autohide: 1,
+    modestbranding: 0,
+    rel: 0,
+    showinfo: 0,
+    controls: 0,
+    disablekb: 1,
+    enablejsapi: 0,
+    iv_load_policy: 3
+  };
+var vid = [{
+    'videoId': '2b5QNj-BVhs',
+    'startSeconds': 515,
+    'endSeconds': 690,
+    'suggestedQuality': 'hd720'
+  }, {
+    'videoId': '9ge5PzHSS0Y',
+    'startSeconds': 465,
+    'endSeconds': 657,
+    'suggestedQuality': 'hd720'
+  }, {
+    'videoId': 'OWsCt7B-KWs',
+    'startSeconds': 0,
+    'endSeconds': 240,
+    'suggestedQuality': 'hd720'
+  }, {
+    'videoId': 'qMR-mPlyduE',
+    'startSeconds': 19,
+    'endSeconds': 241,
+    'suggestedQuality': 'hd720'
+  }],
+  randomvid = Math.floor(Math.random() * (vid.length - 1 + 1));
 
-// The maximum velocity in each direction
-var maxVelocity = 2;
+function onYouTubePlayerAPIReady() {
+  tv = new YT.Player('tv', {
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    },
+    playerVars: playerDefaults
+  });
+}
 
-// The target frames per second (how often do we want to update / redraw the scene)
-var targetFPS = 33;
+function onPlayerReady() {
+  tv.loadVideoById(vid[randomvid]);
+  tv.mute();
+}
 
-// Set the dimensions of the canvas as variables so they can be used.
-var canvasWidth = 400;
-var canvasHeight = 400;
+function onPlayerStateChange(e) {
+  if (e.data === 1) {
+    $('#tv').addClass('active');
+  } else if (e.data === 0) {
+    tv.seekTo(vid[randomvid].startSeconds)
+  }
+}
 
-// Create an image object (only need one instance)
-var imageObj = new Image();
+function vidRescale() {
 
-// Once the image has been downloaded then set the image on all of the particles
-imageObj.onload = function() {
-    particles.forEach(function(particle) {
-            particle.setImage(imageObj);
+  var w = $(window).width() + 200,
+    h = $(window).height() + 200;
+
+  if (w / h > 16 / 9) {
+    tv.setSize(w, w / 16 * 9);
+    $('.tv .screen').css({
+      'left': '0px'
     });
-};
-
-// Once the callback is arranged then set the source of the image
-imageObj.src = "http://www.blog.jonnycornwell.com/wp-content/uploads/2012/07/Smoke10.png";
-
-// A function to create a particle object.
-function Particle(context) {
-
-    // Set the initial x and y positions
-    this.x = 0;
-    this.y = 0;
-
-    // Set the initial velocity
-    this.xVelocity = 0;
-    this.yVelocity = 0;
-
-    // Set the radius
-    this.radius = 5;
-
-    // Store the context which will be used to draw the particle
-    this.context = context;
-
-    // The function to draw the particle on the canvas.
-    this.draw = function() {
-        
-        // If an image is set draw it
-        if(this.image){
-            this.context.drawImage(this.image, this.x-128, this.y-128);         
-            // If the image is being rendered do not draw the circle so break out of the draw function                
-            return;
-        }
-        // Draw the circle as before, with the addition of using the position and the radius from this object.
-        this.context.beginPath();
-        this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        this.context.fillStyle = "rgba(0, 255, 255, 1)";
-        this.context.fill();
-        this.context.closePath();
-    };
-
-    // Update the particle.
-    this.update = function() {
-        // Update the position of the particle with the addition of the velocity.
-        this.x += this.xVelocity;
-        this.y += this.yVelocity;
-
-        // Check if has crossed the right edge
-        if (this.x >= canvasWidth) {
-            this.xVelocity = -this.xVelocity;
-            this.x = canvasWidth;
-        }
-        // Check if has crossed the left edge
-        else if (this.x <= 0) {
-            this.xVelocity = -this.xVelocity;
-            this.x = 0;
-        }
-
-        // Check if has crossed the bottom edge
-        if (this.y >= canvasHeight) {
-            this.yVelocity = -this.yVelocity;
-            this.y = canvasHeight;
-        }
-        
-        // Check if has crossed the top edge
-        else if (this.y <= 0) {
-            this.yVelocity = -this.yVelocity;
-            this.y = 0;
-        }
-    };
-
-    // A function to set the position of the particle.
-    this.setPosition = function(x, y) {
-        this.x = x;
-        this.y = y;
-    };
-
-    // Function to set the velocity.
-    this.setVelocity = function(x, y) {
-        this.xVelocity = x;
-        this.yVelocity = y;
-    };
-    
-    this.setImage = function(image){
-        this.image = image;
-    }
-}
-
-// A function to generate a random number between 2 values
-function generateRandom(min, max){
-    return Math.random() * (max - min) + min;
-}
-
-// The canvas context if it is defined.
-var context;
-
-// Initialise the scene and set the context if possible
-function init() {
-    var canvas = document.getElementById('myCanvas');
-    if (canvas.getContext) {
-
-        // Set the context variable so it can be re-used
-        context = canvas.getContext('2d');
-
-        // Create the particles and set their initial positions and velocities
-        for(var i=0; i < particleCount; ++i){
-            var particle = new Particle(context);
-            
-            // Set the position to be inside the canvas bounds
-            particle.setPosition(generateRandom(0, canvasWidth), generateRandom(0, canvasHeight));
-            
-            // Set the initial velocity to be either random and either negative or positive
-            particle.setVelocity(generateRandom(-maxVelocity, maxVelocity), generateRandom(-maxVelocity, maxVelocity));
-            particles.push(particle);            
-        }
-    }
-    else {
-        alert("Please use a modern browser");
-    }
-}
-
-// The function to draw the scene
-function draw() {
-    // Clear the drawing surface and fill it with a black background
-    context.fillStyle = "rgba(0, 0, 0, 0.5)";
-    context.fillRect(0, 0, 400, 400);
-
-    // Go through all of the particles and draw them.
-    particles.forEach(function(particle) {
-        particle.draw();
+  } else {
+    tv.setSize(h / 9 * 16, h);
+    $('.tv .screen').css({
+      'left': -($('.tv .screen').outerWidth() - w) / 2
     });
+  }
 }
 
-// Update the scene
-function update() {
-    particles.forEach(function(particle) {
-        particle.update();
-    });
-}
+$(window).on('load resize', function() {
+  vidRescale();
+});
 
-// Initialize the scene
-init();
-
-// If the context is set then we can draw the scene (if not then the browser does not support canvas)
-if (context) {
-    setInterval(function() {
-        // Update the scene befoe drawing
-        update();
-
-        // Draw the scene
-        draw();
-    }, 1000 / targetFPS);
-}
+$('.hi span').on('click', function() {
+  $('#tv').toggleClass('mute');
+  if ($('#tv').hasClass('mute')) {
+    tv.mute();
+  } else {
+    tv.unMute();
+  }
+});
