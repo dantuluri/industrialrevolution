9a5d2a8e2a14ec2154ee324eed6ec875eb8c78dd
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-09 21:14:12 -0800

    a

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,39 @@ var unitlessValue = anime({
   translateX: 250,
   rotate: 540
 });
+var timelineParameters = anime.timeline({
+  direction: 'alternate',
+  loop: true
+});
+
+var timelineParameters = anime.timeline({
+  direction: 'alternate',
+  loop: true
+});
+
+timelineParameters
+  .add({
+    targets: '#timelineParameters .square.el',
+    translateX: [ { value: 50 }, { value: 75 }, { value: 100 } ],
+    translateY: [ { value: 10 }, { value: 15 }, { value: 80 } ],
+    duration: 3000
+  })
+  .add({
+    targets: '#timelineParameters .circle.el',
+    translateX: [ { value: 30 }, { value: 60 }, { value: 100 } ],
+    translateY: [ { value: 20 }, { value: 5 }, { value: 5 } ],
+    duration: 3000,
+    offset: 200
+  })
+  .add({
+    targets: '#timelineParameters .triangle.el',
+    translateX: [ { value: 80/2 }, { value: 250/2 } ],
+    translateY: [ { value: -40 }, { value: -10 }, { value: -10 } ],
+    duration: 3000,
+    offset: 400
+  });
+
+
+
+;
+
