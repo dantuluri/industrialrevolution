13b24d757b05f394d4e693ea4ca4bd45a2ec7594
Surya Dantuluri <surya.mvhs@gmail.com>
2017-11-10 16:07:22 -0800

    d

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -151,3 +151,38 @@ for (var i = 0; i < pathEls.length; i++) {
 }
 
 
+
+
+anime({
+  targets: 'path',
+  strokeDashoffset: function(el) {
+    var pathLength = el.getTotalLength();
+    el.setAttribute('stroke-dasharray', pathLength);
+    return [-pathLength, 0];
+  },
+  stroke: {
+    value: function(el, i) {
+      return 'rgb(200,'+ i * 8 +',150)'; 
+    },
+    easing: 'linear',
+    duration: 2000,
+  },
+  strokeWidth: {
+    value: 6,
+    easing: 'linear',
+    delay: function(el, i) { 
+      return 1200 + (i * 40); 
+    },
+    duration: 800,
+  },
+  delay: function(el, i) { 
+    return i * 60; 
+  },
+  duration: 1200,
+  easing: 'easeOutExpo',
+  loop: true,
+  direction: 'alternate'
+});
+
+
+
